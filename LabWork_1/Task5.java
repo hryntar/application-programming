6ca1cc775111ae038446740bb9b0725ba1daa4d3
hryntar <hrynchuktt@gmail.com>
2024-09-22 11:30:45 +0300

    task 5 refactoring

--- LabWork_1/Task5.java
+++ LabWork_1/Task5.java
@@ -2,35 +2,28 @@ package LabWork_1;
 
 public class Task5 {
    public static void main(String[] args) {
-      String[][] matrix = {
+      final String[][] matrix = {
             { "abc", "def", "ghi" },
-            { "jkl", "mno", "pqr" },
-            { "stu", "vwx", "yno" }
+            { "jkl", "mno", "pqr nof" },
+            { "stu", "vwx", "ynono" }
       };
 
-      String substring = "no";
+      final String substring = "no";
 
-      int occurrences = countSubstringInMatrix(matrix, substring);
+      int occurrences = countSubstrings(matrix, substring);
       System.out.println("Occurrences of '" + substring + "': " + occurrences);
    }
 
-   public static int countSubstringInMatrix(String[][] matrix, String substring) {
+   public static int countSubstrings(String[][] matrix, String substring) {
       int count = 0;
-
-      for (int i = 0; i < matrix.length; i++) {
-         String row = String.join("", matrix[i]);
-         count += countOccurrences(row, substring);
-      }
-
-      return count;
-   }
-
-   public static int countOccurrences(String text, String substring) {
-      int count = 0;
-      int index = 0;
-      while ((index = text.indexOf(substring, index)) != -1) {
-         count++;
-         index += substring.length();
+      for (String[] row : matrix) {
+         for (String str : row) {
+            int pos = str.indexOf(substring);
+            while (pos != -1) {
+               count++;
+               pos = str.indexOf(substring, pos + 1);
+            }
+         }
       }
       return count;
    }
